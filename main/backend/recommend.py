34016948490104eb0504dad1ea0b407881355625
Rohit-prgms <158438750+Rohit-prgms@users.noreply.github.com>
2024-05-19 17:34:33 +0530

    recommend.py

--- main/backend/recommend.py
+++ main/backend/recommend.py
@@ -19,25 +19,14 @@ def crop_yeild(request,cropS):
     return y_pred
     
 def crop_recommend(request):
-#    ic(request)
+
    input_df = pd.DataFrame([request])
    ic(input_df)
    input_df['Season']=label_encoder.fit_transform(input_df['Season'])
    input_df['State']=label_encoder.fit_transform(input_df['State'])
    y_pred = load_model_R.predict(input_df)
-#    print(y_pred[0])
+
    return y_pred[0]
-
-
-
-# crop_recommend({
-#     "Crop_Year": 2024,
-#     "Season": "Kharif",
-#     "State": "Tamil Nadu",
-#     "Area": 95217.0,
-#     "Production": 35885,
-#     "Annual_Rainfall":315.9
-# })
 
 a=crop_yeild({
     "Crop_Year": 2024,
@@ -49,4 +38,3 @@ a=crop_yeild({
     },"Urad")
 
 print(a)
-#output {'Fertilizer': 12.1582367994, 'Pesticide': 16339137.180100005, 'Yield': 34844.3036}
